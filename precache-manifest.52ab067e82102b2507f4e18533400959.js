ab85d52ba5fbaa72fcddfd7b04db091ceb377b7c
Andy Bleakley <andy@Andys-MacBook-Pro.local>
2020-08-24 00:22:39 +0100

    Updates

---
+++ precache-manifest.52ab067e82102b2507f4e18533400959.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d19b9bc3d999c3f5309cda47741a5b8",
+    "url": "/clicky/index.html"
+  },
+  {
+    "revision": "5278fc64bbee32216068",
+    "url": "/clicky/static/css/main.5db42a40.chunk.css"
+  },
+  {
+    "revision": "eea93a96b3668bc30416",
+    "url": "/clicky/static/js/2.6fb57f59.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/clicky/static/js/2.6fb57f59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5278fc64bbee32216068",
+    "url": "/clicky/static/js/main.233a9210.chunk.js"
+  },
+  {
+    "revision": "9868e024bc5f9d90297f",
+    "url": "/clicky/static/js/runtime-main.8db5a474.js"
+  },
+  {
+    "revision": "183d8168dd33f322af8e07cbffc569db",
+    "url": "/clicky/static/media/elena-prokofyeva-17909-unsplash-min.183d8168.jpg"
+  },
+  {
+    "revision": "bd3264189359631d6a278ee419918284",
+    "url": "/clicky/static/media/jordan-mcqueen-99269-unsplash-min.bd326418.jpg"
+  },
+  {
+    "revision": "c230c34a6d7ef12289704e176034d991",
+    "url": "/clicky/static/media/kalen-emsley-98264-unsplash-min.c230c34a.jpg"
+  },
+  {
+    "revision": "76a2d70ffcb3e0644d2fdb14a94c3df7",
+    "url": "/clicky/static/media/michael-dam-258165-unsplash-min.76a2d70f.png"
+  },
+  {
+    "revision": "b0d08f7947cf79e8f3efaf66d8de6af3",
+    "url": "/clicky/static/media/proximanova_bold.b0d08f79.woff2"
+  },
+  {
+    "revision": "979672d1ee68029e8a3a9bc6f4a87519",
+    "url": "/clicky/static/media/proximanova_light.979672d1.woff2"
+  },
+  {
+    "revision": "e91e32ca46944c774b29d452db172494",
+    "url": "/clicky/static/media/proximanova_regular.e91e32ca.woff2"
+  },
+  {
+    "revision": "c01a0f0dce3a8650f10a7543fde2ac52",
+    "url": "/clicky/static/media/proximanova_semibold.c01a0f0d.woff2"
+  },
+  {
+    "revision": "88bb859efa47a04c80b1b44e503d6cbf",
+    "url": "/clicky/static/media/riccardo-chiarini-365677-unsplash-min.88bb859e.jpg"
+  }
+]);
